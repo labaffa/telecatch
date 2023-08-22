1b5f2205ec33e1ab9deece8fbbae49334f2b6155
labaffa <dellarovere.lante@gmail.com>
2023-08-23 00:41:01 +0200

    update models

--- teledash/models.py
+++ teledash/models.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel
-from typing import Union
+from pydantic import BaseModel, Field, EmailStr
+from typing import Union, Optional, List
+from uuid import UUID, uuid4
 
 
 def validate_int(v):
@@ -41,4 +42,63 @@ class Message(BaseModel):
     country: Union[str, None]
     views: Union[StringInt, None]
     # media: Union[str, None]
+
+
+# login
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+
+class TokenPayload(BaseModel):
+    sub: str = None
+    exp: int = None
+
+
+class UserAuth(BaseModel):
+    email: EmailStr = Field(..., description="user email")
+    username: str = Field(..., min_length=5, max_length=50, description="user username")
+    password: str = Field(..., min_length=5, max_length=24, description="user password")
+    
+
+class TelegramClient(BaseModel):
+    phone: Union[str, None] = None
+    api_id: Union[str, None] = None
+    api_hash: Union[str, None] = None
+    client_id: str  # this is session_path (phone + id + hash)
+    authenticated: bool = False
+
+
+class User(BaseModel):
+    user_id: str
+    username: str
+    email: EmailStr
+    displayname: Union[str, None] = None
+    first_name: Union[str, None] = None
+    last_name: Union[str, None] = None
+    disabled: Optional[bool] = False
+    
+    
+class UserTelegramRelation(BaseModel):
+    user_id: str
+    client_id: str
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+class UserUpdate(BaseModel):
+    email: Optional[EmailStr] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+
+
+
 
