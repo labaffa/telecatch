84a9d4037e57e5de25f896408fe9e818eb275127
labaffa <dellarovere.lante@gmail.com>
2023-09-13 19:35:24 +0200

    Update sqlalchemy models

--- teledash/db/models.py
+++ teledash/db/models.py
@@ -9,61 +9,112 @@ from sqlalchemy.sql import func
 Base = declarative_base()
 
 
-class User(Base):
+class MyBase(Base):
+    __abstract__ = True
+
+    def to_dict(self):
+        return {field.name:getattr(self, field.name) for field in self.__table__.c}
+
+
+class User(MyBase):
     __tablename__ = "user"
+    __table_args__ = (
+       UniqueConstraint("username", name="user_username"),
+       UniqueConstraint("email", name="user_email")
+    )
+
     id = Column(Integer, primary_key=True)
     username = Column(Text, nullable=False)
     email = Column(Text, nullable=False)
+    hashed_password = Column(Text, nullable=False)
     displayname = Column(Text, nullable=True, default="")
     first_name = Column(Text, nullable=True, default="")
     last_name = Column(Text, nullable=True, default="")
     disabled = Column(Boolean, default=False)
-    hashed_password = Column(Text, nullable=True)
 
 
-class ChannelCommon(Base):
+class ChannelCommon(MyBase):
     __tablename__ = "channel_common"
-    id = Column(Integer, primary_key=True)
-    url = Column(Text, nullable=False)
+    __table_args__ = (
+       UniqueConstraint("id", name="channel_id"),
+    )
+
+    id = Column(Integer, nullable=True)
+    url = Column(Text, primary_key=True, nullable=False, autoincrement=False)
+    username = Column(Text, nullable=True)
     type = Column(Text, nullable=False)
-    access_hash = Column(Text)
-    messages_count = Column(Text)
-    participants_count = Column(Text)
+    access_hash = Column(Integer, nullable=True)
+    messages_count = Column(Integer, nullable=True)
+    participants_count = Column(Integer, nullable=True)
     about = Column(Text, default="")
     title = Column(Text, default="")
     inserted_at = Column(DateTime)
     updated_at = Column(DateTime)
 
 
-class Tag(Base):
+class Tag(MyBase):
     __tablename__ = "tag"
     id = Column(Integer, primary_key=True)
     name = Column(Text, nullable=False)
 
 
-class ChannelCustom(Base):
+class ChannelCustom(MyBase):
     __tablename__ = "channel_custom"
-    channel_id = Column(Integer, ForeignKey("channel_common.id"))
-    user_id = Column(Integer, ForeignKey("user.id"))
+    channel_url = Column(Text, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
     language = Column(Text, nullable=True)
     location = Column(Text, nullable=True)
     category = Column(Text, nullable=True)
+    is_joined = Column(Boolean, default=False)
 
 
-class ChannelTag(Base):
+class ChannelTag(MyBase):
     __tablename__ = "channel_tag"
 
-    channel_id = Column(Integer, ForeignKey("channel_common.id"))
-    user_id = Column(Integer, ForeignKey("user.id"))
-    tag_id = Column(Integer, ForeignKey("tag.id")) 
+    channel_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
+    tag_id = Column(Integer, primary_key=True) 
 
 
-class TgClient(Base):
+class TgClient(MyBase):
     __tablename__ = "tg_client"
 
-    id = Column(Text, primary_key=True)
+    id = Column(Text, primary_key=True, autoincrement=False)
     phone = Column(Text, nullable=False)
     authenticated = Column(Boolean, default=False)
+    api_id = Column(Integer, nullable=True)
+    api_hash = Column(Text, nullable=True)
+    # api_id_last_2 = Column(Text, nullable=True)
+    # api_hash_last_2 = Column(Text, nullable=True)
 
 
+class UserClient(MyBase):
+    __tablename__ = "user_client"
 
+    user_id = Column(Integer, primary_key=True)
+    client_id = Column(Text, primary_key=True)
+
+
+class ChannelCollection(MyBase):
+    __tablename__ = "channel_collection"
+
+    collection_title = Column(Text, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
+    channel_url = Column(Text, primary_key=True)
+
+
+class CollectionJob(MyBase):
+    __tablename__ = "collection_job"
+
+    uid = Column(Text, primary_key=True, autoincrement=False)
+    user_id = Column(Integer)
+    collection_title = Column(Text)
+    status = Column(Text, default="in_progress")
+    processed_channels = Column(Text, default="[]")
+
+
+class ActiveCollection(MyBase):
+    __tablename__ = "active_collection"
+
+    user_id = Column(Integer, primary_key=True, autoincrement=False)
+    collection_title = Column(Text, primary_key=True)
