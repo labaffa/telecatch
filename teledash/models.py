11ca517a54453fb22883a4383f1796d4bb85c666
labaffa <dellarovere.lante@gmail.com>
2023-09-13 19:30:47 +0200

    Update pydantic schemas

--- teledash/models.py
+++ teledash/models.py
@@ -1,6 +1,8 @@
-from pydantic import BaseModel, Field, EmailStr
-from typing import Union, Optional, List, Literal
+from pydantic import BaseModel, Field, EmailStr, Json
+from typing import Union, Optional, List, Literal, Any
 from uuid import UUID, uuid4
+from dateutil.parser import parse
+import datetime as dt
 
 
 def validate_int(v):
@@ -9,14 +11,29 @@ def validate_int(v):
     return int(v)
 
 
+def validate_date(v):
+    if not v:
+        return None
+    return parse(v)
+
+
 class StringInt(str):
     @classmethod
     def __get_validators__(cls):
         yield validate_int
 
 
+class StrictDate(dt.datetime):
+    @classmethod
+    def __get_validators__(cls):
+        yield validate_date
+
+
 class ChannelCreate(BaseModel):
-    identifier: Union[str, int]
+    url: Union[str, int]
+
+    def __hash__(self):
+        return hash(self.url)
 
 
 class Channel(ChannelCreate):
@@ -69,14 +86,14 @@ class UserAuth(BaseModel):
 
 class TelegramClient(BaseModel):
     phone: Union[str, None] = None
-    api_id: Union[str, None] = None
-    api_hash: Union[str, None] = None
+    api_id: int
+    api_hash: str
     client_id: str  # this is session_path (phone + id + hash)
     authenticated: bool = False
 
 
 class User(BaseModel):
-    user_id: str
+    id: int
     username: str
     email: EmailStr
     displayname: Union[str, None] = None
@@ -86,12 +103,15 @@ class User(BaseModel):
     
     
 class UserTelegramRelation(BaseModel):
-    user_id: str
+    user_id: int
     client_id: str
 
 
 class UserInDB(User):
     hashed_password: str
+
+    class Config:
+        orm_mode = True
 
 
 class UserUpdate(BaseModel):
@@ -111,29 +131,41 @@ class ChannelUpload(BaseModel):
 
 
 class ChannelCommon(BaseModel):
-    id: int
+    id: Union[int, None] = None
     url: str
-    username: str
-    type: Literal["channel", "group"]
+    username: Union[str, None] = None
+    type: Literal["channel", "group", "undefined"] = "undefined"
     access_hash: Union[int, None] = None
     messages_count: Union[int, None] = None
     participants_count: Union[int, None] = None
     about: str = ""
-    title: str = Field(..., alias="name")
-    inserted_at: str
-    updated_at: str
+    title: str = Field(validation_alias="name", default="")
+    inserted_at: dt.datetime = Field(default_factory=dt.datetime.utcnow)
+    updated_at: dt.datetime = Field(default_factory=dt.datetime.utcnow)
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
 
 
-class ChannelCustom(BaseModel):
-    channel_id: int
-    user_id: str
-    language: str
-    location: str
-    category: str
+class ChannelCustomCreate(BaseModel):
+    channel_url: str = Field(..., validation_alias="url")
+    language: Optional[str]
+    location: Optional[str]
+    category: Optional[str]
+
+
+class ChannelCustom(ChannelCustomCreate):
+    user_id: int
+    
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
 
 
 class ChannelTag(BaseModel):
-    user_id: str
+    user_id: int
     channel_id: int
     tag_id: int = 0  # index for meaningless/default tag
 
@@ -143,7 +175,38 @@ class Tag(BaseModel):
     tag_id: int
 
 
+class ChannelCollection(BaseModel):
+    collection_title: str
+    user_id: int
+    channel_url: str
 
 
+class ChannelCollectionPayload(BaseModel):
+    """ user_id is taken from fastapi dependency"""
+
+    collection_title: str
+    channel_urls: List[ChannelCreate]
 
 
+class Job(BaseModel):
+    uid: UUID = Field(default_factory=uuid4)
+    status: str = "in_progress"
+    processed_channels: List[str] = Field(default_factory=list)
+
+
+class CollectionJob(BaseModel):
+    uid: str
+    user_id: int
+    collection_title: str
+    status: str = "in_progress"
+    processed_channels: Json[Any] = '[]'
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+
+
+class ActiveCollection(BaseModel):
+    user_id: int
+    collection_title: str
+    
