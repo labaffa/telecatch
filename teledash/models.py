7481ce5640ab67018d9b4fe598961f29ae83a65a
labaffa <dellarovere.lante@gmail.com>
2023-09-26 11:17:10 +0200

    Change the min number of chars for username and password to 1

--- teledash/models.py
+++ teledash/models.py
@@ -80,8 +80,8 @@ class TokenPayload(BaseModel):
 
 class UserAuth(BaseModel):
     email: EmailStr = Field(..., description="user email")
-    username: str = Field(..., min_length=5, max_length=50, description="user username")
-    password: str = Field(..., min_length=5, max_length=24, description="user password")
+    username: str = Field(..., min_length=1, max_length=50, description="user username")
+    password: str = Field(..., min_length=1, max_length=24, description="user password")
     
 
 class TelegramClient(BaseModel):
