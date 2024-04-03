7da299bb05c3cf71a37ca0d0224c650e4b066bc6
labaffa <dellarovere.lante@gmail.com>
2024-04-03 17:42:29 +0200

    Add endpoints for search router, with /search and /export_search

--- teledash/api/search.py
+++ teledash/api/search.py
@@ -1,18 +1,14 @@
 import fastapi
 from fastapi import HTTPException, APIRouter, Depends, Query
-from fastapi.responses import HTMLResponse, StreamingResponse, \
-    FileResponse
+from fastapi.responses import StreamingResponse
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from teledash.channel_messages import search_all_channels, \
     search_all_channels_generator, download_all_channels_media
 import base64
-from teledash.config import DEFAULT_CHANNELS
 from typing import Union
-from teledash import models
-from teledash import config
-import datetime as dt
-from telethon import types
+from teledash import schemas as schemas
+from teledash.db import models
 from typing import List
 import io
 import pandas as pd
@@ -22,10 +18,13 @@ import io
 import csv
 from teledash.utils import telegram
 from sqlalchemy.orm import Session
-from teledash.db.db_setup import get_db
-from teledash.utils.db import tg_client as ut
+from teledash.db.db_setup import get_async_session
 from teledash.utils.db import channel as uc
 from teledash.utils.db import user as uu
+from teledash.utils.users import active_user
+from typing_extensions import Annotated
+from pydantic import AfterValidator
+import zipfile
 
 
 search_router = APIRouter()
@@ -37,53 +36,73 @@ def validate_date(v):
     return parse(v)
 
 
-class StrictDate(dt.datetime):
-    @classmethod
-    def __get_validators__(cls):
-        yield validate_date
+def validate_format(v):
+    if not v:
+        v = "tsv"  # default
+    v = v.strip().lower()
+    valid_formats = ["json", "tsv"]
+    if v not in valid_formats:
+        raise ValueError(f"{v} format is not supported.")
+    return v
 
 
-@search_router.get("/api/search_channels")
+StrictDate = Annotated[
+    str | None,
+    AfterValidator(lambda x: validate_date(x))
+]
+
+
+OutFormat = Annotated[
+    str | None,
+    AfterValidator(lambda x: validate_format(x))
+]
+
+
+@search_router.get("/search")
 async def read_search_channel(
     request: fastapi.Request,
-    search,
+    search: str | None,
+    collection_title: str = None,
     channel_urls: List[str]=Query(default=[]),
     start_date: StrictDate=None,
     end_date: StrictDate=None,
     chat_type: Union[str, None]=None,
-    country: Union[str, None]=None,
     limit: int=100, 
     offset_channel: int=0, 
     offset_id: int=0,
-    db: Session=Depends(get_db),
-    user = Depends(config.settings.MANAGER),
-    client_id=None
+    db: Session=Depends(get_async_session),
+    user: models.User = Depends(active_user),
+    client_id: str | None=None
 ):
+    if limit > 100:
+        limit = 100
+    # collection_title overrides channel_urls
+    if collection_title is not None:
+        collection_in_db = await uc.get_channel_collection(db, user.id, collection_title)
+        if not collection_in_db:
+            raise fastapi.HTTPException(
+                status_code=400, 
+                detail=(f"'{collection_title}' collection not present in user account. "
+                        "Create it or chose another collection"
+                )
+            )
+        channel_urls = [x["channel_url"] for x in collection_in_db]
     if not client_id:
-        client_id = uu.get_active_client(db, user.id)
+        client_id = await uu.get_active_client(db, user.id)
     if client_id is None:
         raise HTTPException(
             status_code=400,
             detail=f"User {user.username} has not registered Telegram clients"
         )
     tg_client = request.app.state.clients.get(client_id)
-    if tg_client is None:
+    if tg_client is None:  # I believe we should test if tg_client works 
         tg_client = await telegram.get_authenticated_client(db, client_id)
         request.app.state.clients[client_id] = tg_client
     if not channel_urls:
-        # collection_titles = uc.get_channel_collection_titles_of_user(db, int(user.id))
-        # title = collection_titles[0] if collection_titles else None
-        # if title is None:
         raise HTTPException(
             status_code=400,
             detail=f"Please set channels to search, set or create a collection"
         )
-        # # TODO: this should be replaced so that 'search_all_channels' does not query the db
-        # channel_urls = [   
-        #     x["channel_url"] 
-        #     for x in uc.get_channel_collection(db, user.id, title)
-        # ]
-    
     try:
         response = await search_all_channels(
             db=db,
@@ -93,7 +112,6 @@ async def read_search_channel(
             start_date=start_date,
             end_date=end_date,
             chat_type=chat_type,
-            country=country,
             limit=limit,
             offset_channel=offset_channel,
             offset_id=offset_id,
@@ -109,123 +127,37 @@ async def read_search_channel(
         )
 
 
-@search_router.get("/api/stream_search")
-async def search_and_export_messages_to_csv(
-    request: fastapi.Request,
-    search: str,
-    channel_urls: List[str]=Query(default=[]),
-    start_date: StrictDate=None,
-    end_date: StrictDate=None,
-    chat_type: Union[str, None]=None,
-    country: Union[str, None]=None,
-    limit: int=100, 
-    offset_channel: int=0, 
-    offset_id: int=0,
-    out_format: Union[str, None]=None,
-    db: Session=Depends(get_db),
-    user = Depends(config.settings.MANAGER),
-    client_id = None
-):
-    if not client_id:
-        client_id = uu.get_active_client(db, user.id)
-    if client_id is None:
-        raise HTTPException(
-            status_code=400,
-            detail=f"User {user.username} has not registered Telegram clients"
-        )
-    
-    tg_client = request.app.state.clients.get(client_id)
-    if tg_client is None:
-        tg_client = await telegram.get_authenticated_client(db, client_id)
-        request.app.state.clients[client_id] = tg_client
-    if not channel_urls:
-        collection_titles = uc.get_channel_collection_titles_of_user(db, int(user.id))
-        title = collection_titles[0] if collection_titles else None
-        if title is None:
-            raise HTTPException(
-            status_code=400,
-            detail=f"Please set channels to search or create a collection"
-        )
-        # TODO: this should be replaced so that 'search_all_channels' does not query the db
-        channel_urls = [   
-            x["channel_url"] 
-            for x in uc.get_channel_collection(db, user.id, title)
-        ]
-    headers = {}
-    if (out_format == "tsv") or (out_format == "json"):
-        fext = ".tsv" if out_format == "tsv" else ".json"
-        headers = {
-            'Access-Control-Expose-Headers': 'Content-Disposition',
-            'Content-Disposition': f'attachment; filename="export{fext}"'
-    }
-    results = search_all_channels_generator(
-            db=db,
-            client=tg_client, 
-            search=search,
-            channel_urls=channel_urls,
-            start_date=start_date,
-            end_date=end_date,
-            chat_type=chat_type,
-            country=country,
-            limit=limit,
-            offset_channel=offset_channel,
-            offset_id=offset_id,
-            user_id=user.id
-        )
-    
-    async def _encoded_results():
-        fieldnames = models.Message.__fields__.keys()
-        stream = io.StringIO()
-        writer = csv.DictWriter(stream, fieldnames=fieldnames, delimiter="\t")
-        if out_format == "tsv":
-            writer.writeheader()
-            yield stream.getvalue()
-            async for item in results:
-                stream.truncate(0)
-                stream.seek(0)
-                writer.writerow(item)
-                yield stream.getvalue()
-        elif out_format =="json":
-            idx = 0
-            yield "["
-            async for item in results:
-                if idx > 0:
-                    yield ","
-                yield json.dumps(
-                    jsonable_encoder(models.Message(**item).dict())
-                )
-                idx += 1
-            yield "]"
-    return StreamingResponse(
-        content=_encoded_results(),
-        media_type='application/octet-stream',
-        headers=headers
-    )
-
-
-@search_router.get("/api/stream_search_with_media")
+@search_router.get("/export_search")
 async def search_and_export_messages_and_media_to_zip_file(
     request: fastapi.Request,
     search: str,
+    collection_title: str | None = None,
     channel_urls: List[str]=Query(default=[]),
     start_date: StrictDate=None,
     end_date: StrictDate=None,
     chat_type: Union[str, None]=None,
-    country: Union[str, None]=None,
     limit: int=100, 
     offset_channel: int=0, 
     offset_id: int=0,
-    out_format: Union[str, None]=None,
-    db: Session=Depends(get_db),
-    user = Depends(config.settings.MANAGER),
+    out_format: OutFormat="tsv",
+    db: Session=Depends(get_async_session),
+    user: models.User = Depends(active_user),
     client_id = None,
-    media: bool=False
+    with_media: bool=True
 ):
-    import zipfile
-    
+    if collection_title is not None:
+        collection_in_db = await uc.get_channel_collection(db, user.id, collection_title)
+        if not collection_in_db:
+            raise fastapi.HTTPException(
+                status_code=400, 
+                detail=(f"'{collection_title}' collection not present in user account. "
+                        "Create it or chose another collection"
+                )
+            )
+        channel_urls = [x["channel_url"] for x in collection_in_db]
 
     if not client_id:
-        client_id = uu.get_active_client(db, user.id)
+        client_id = await uu.get_active_client(db, user.id)
     if client_id is None:
         raise HTTPException(
             status_code=400,
@@ -237,23 +169,14 @@ async def search_and_export_messages_and_media_to_zip_file(
         tg_client = await telegram.get_authenticated_client(db, client_id)
         request.app.state.clients[client_id] = tg_client
     if not channel_urls:
-        collection_titles = uc.get_channel_collection_titles_of_user(db, int(user.id))
-        title = collection_titles[0] if collection_titles else None
-        if title is None:
-            raise HTTPException(
+        raise HTTPException(
             status_code=400,
             detail=f"Please set channels to search or create a collection"
         )
-        # TODO: this should be replaced so that 'search_all_channels' does not query the db
-        channel_urls = [   
-            x["channel_url"] 
-            for x in uc.get_channel_collection(db, user.id, title)
-        ]
-    
-    if media:
+    if with_media:
         headers = {
             'Access-Control-Expose-Headers': 'Content-Disposition',
-            'Content-Disposition': f'attachment; filename="export.zip"'
+            f'Content-Disposition': f'attachment; filename="media_export.zip"'
         }
         results = download_all_channels_media(
             db=db,
@@ -263,7 +186,6 @@ async def search_and_export_messages_and_media_to_zip_file(
             start_date=start_date,
             end_date=end_date,
             chat_type=chat_type,
-            country=country,
             limit=limit,
             offset_channel=offset_channel,
             offset_id=offset_id,
@@ -271,6 +193,7 @@ async def search_and_export_messages_and_media_to_zip_file(
             user_id=user.id
         )
         async def _encoded_results():
+            tsv_columns = schemas.Message.__fields__.keys()
             zip_buffer = io.BytesIO()
             seek_pos = 0
             with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as z:
@@ -278,11 +201,14 @@ async def search_and_export_messages_and_media_to_zip_file(
                     if item["type"] == "media":
                         with z.open(f'media/{item["filename"]}', mode='w') as mediafile:
                             mediafile.write(item["data"])
-                
-                        zip_buffer.seek(seek_pos)
-                        buffer_chunk = zip_buffer.read()
-                        seek_pos = zip_buffer.tell()
-                        yield buffer_chunk
+                    elif item["type"] == "messages":
+                        df = pd.DataFrame(item["data"], columns=tsv_columns)
+                        with z.open(f'messages/{item["filename"]}', mode='w') as mediafile:
+                            mediafile.write(df.to_csv(sep="\t", index=False).encode())
+                    zip_buffer.seek(seek_pos)
+                    buffer_chunk = zip_buffer.read()
+                    seek_pos = zip_buffer.tell()
+                    yield buffer_chunk
             zip_buffer.seek(seek_pos)
             yield zip_buffer.read()
 
@@ -293,12 +219,10 @@ async def search_and_export_messages_and_media_to_zip_file(
             headers=headers
         )
     else:
-        headers = {}
-        if (out_format == "tsv") or (out_format == "json"):
-            fext = ".tsv" if out_format == "tsv" else ".json"
-            headers = {
-                'Access-Control-Expose-Headers': 'Content-Disposition',
-                'Content-Disposition': f'attachment; filename="export{fext}"'
+        
+        headers = {
+            'Access-Control-Expose-Headers': 'Content-Disposition',
+            'Content-Disposition': f'attachment; filename="export.{out_format}"'
         }
         results = search_all_channels_generator(
             db=db,
@@ -308,7 +232,6 @@ async def search_and_export_messages_and_media_to_zip_file(
             start_date=start_date,
             end_date=end_date,
             chat_type=chat_type,
-            country=country,
             limit=limit,
             offset_channel=offset_channel,
             offset_id=offset_id,
@@ -316,7 +239,7 @@ async def search_and_export_messages_and_media_to_zip_file(
         )
     
         async def _encoded_results():
-            fieldnames = models.Message.__fields__.keys()
+            fieldnames = schemas.Message.__fields__.keys()
             stream = io.StringIO()
             writer = csv.DictWriter(stream, fieldnames=fieldnames, delimiter="\t")
             if out_format == "tsv":
@@ -334,7 +257,7 @@ async def search_and_export_messages_and_media_to_zip_file(
                     if idx > 0:
                         yield ","
                     yield json.dumps(
-                        jsonable_encoder(models.Message(**item).dict())
+                        jsonable_encoder(schemas.Message(**item).model_dump())
                     )
                     idx += 1
                 yield "]"
