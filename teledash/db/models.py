4d10acc9f93339b46a87e2df272edef32bc40091
labaffa <dellarovere.lante@gmail.com>
2024-04-03 17:37:57 +0200

    Fix: update models and align them to alembic versions

--- teledash/db/models.py
+++ teledash/db/models.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import null
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
+import fastapi_users_db_sqlalchemy
 
 
 Base = declarative_base()
@@ -16,22 +18,34 @@ class MyBase(Base):
         return {field.name:getattr(self, field.name) for field in self.__table__.c}
 
 
-class User(MyBase):
+# class User(MyBase):
+#     __tablename__ = "user"
+#     __table_args__ = (
+#        UniqueConstraint("username", name="user_username"),
+#        UniqueConstraint("email", name="user_email")
+#     )
+
+#     id = Column(Integer, primary_key=True)
+#     username = Column(Text, nullable=False)
+#     email = Column(Text, nullable=False)
+#     hashed_password = Column(Text, nullable=False)
+#     displayname = Column(Text, nullable=True, default="")
+#     first_name = Column(Text, nullable=True, default="")
+#     last_name = Column(Text, nullable=True, default="")
+#     disabled = Column(Boolean, default=False)
+
+
+class User(SQLAlchemyBaseUserTableUUID, MyBase):
     __tablename__ = "user"
     __table_args__ = (
        UniqueConstraint("username", name="user_username"),
        UniqueConstraint("email", name="user_email")
     )
-
-    id = Column(Integer, primary_key=True)
     username = Column(Text, nullable=False)
-    email = Column(Text, nullable=False)
-    hashed_password = Column(Text, nullable=False)
     displayname = Column(Text, nullable=True, default="")
     first_name = Column(Text, nullable=True, default="")
     last_name = Column(Text, nullable=True, default="")
-    disabled = Column(Boolean, default=False)
-
+    
 
 class ChannelCommon(MyBase):
     __tablename__ = "channel_common"
@@ -61,7 +75,7 @@ class Tag(MyBase):
 class ChannelCustom(MyBase):
     __tablename__ = "channel_custom"
     channel_url = Column(Text, primary_key=True)
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(fastapi_users_db_sqlalchemy.generics.GUID(), primary_key=True)
     language = Column(Text, nullable=True)
     location = Column(Text, nullable=True)
     category = Column(Text, nullable=True)
@@ -72,7 +86,7 @@ class ChannelTag(MyBase):
     __tablename__ = "channel_tag"
 
     channel_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(fastapi_users_db_sqlalchemy.generics.GUID(), primary_key=True)
     tag_id = Column(Integer, primary_key=True) 
 
 
@@ -91,7 +105,7 @@ class TgClient(MyBase):
 class UserClient(MyBase):
     __tablename__ = "user_client"
 
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(fastapi_users_db_sqlalchemy.generics.GUID(), primary_key=True)
     client_id = Column(Text, primary_key=True)
 
 
@@ -99,7 +113,7 @@ class ChannelCollection(MyBase):
     __tablename__ = "channel_collection"
 
     collection_title = Column(Text, primary_key=True)
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(fastapi_users_db_sqlalchemy.generics.GUID(), primary_key=True)
     channel_url = Column(Text, primary_key=True)
 
 
@@ -107,7 +121,7 @@ class CollectionJob(MyBase):
     __tablename__ = "collection_job"
 
     uid = Column(Text, primary_key=True, autoincrement=False)
-    user_id = Column(Integer)
+    user_id = Column(fastapi_users_db_sqlalchemy.generics.GUID())
     collection_title = Column(Text)
     status = Column(Text, default="in_progress")
     processed_channels = Column(Text, default="[]")
@@ -117,14 +131,14 @@ class ActiveCollection(MyBase):
     __tablename__ = "active_collection"
 
     # I think the primary key should be just user_id, because just one for user should be there
-    user_id = Column(Integer, primary_key=True, autoincrement=False)
+    user_id = Column(fastapi_users_db_sqlalchemy.generics.GUID(), primary_key=True, autoincrement=False)
     collection_title = Column(Text, primary_key=True)
 
 
 class ActiveClient(MyBase):
     __tablename__ = "active_client"
 
-    user_id = Column(Integer, primary_key=True, autoincrement=False)
+    user_id = Column(fastapi_users_db_sqlalchemy.generics.GUID(), primary_key=True, autoincrement=False)
     client_id = Column(Text)
 
 
