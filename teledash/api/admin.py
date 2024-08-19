3f5b30f886b3972fe1f433bdb0708b5ed3781ae4
Giosuè Ruscica <dellarovere.lante@gmail.com>
2024-08-19 10:15:32 +0200

    refactor admin api and add endpoint for downloading data

--- teledash/api/admin.py
+++ teledash/api/admin.py
@@ -1,77 +1,28 @@
 from fastapi import APIRouter, Depends, HTTPException
 import fastapi
-from teledash import schemas
-from teledash import config
-from teledash.utils.db import user as uu
-from typing import List
-from sqlalchemy.orm import Session
-from email_validator import validate_email
-from teledash.utils.admin import set_disabled
-from teledash.db.db_setup import get_db
 import os
-try:
-    from typing import Annotated
-except Exception:
-    from typing_extensions import Annotated
+from  teledash import config
+from pathlib import Path
+# try:
+#     from typing import Annotated
+# except Exception:
+#     from typing_extensions import Annotated
 
 
 admin_router = APIRouter()
 
 
 @admin_router.get(
-    '/all_users',
-    summary="Get list of all users in db",
-    response_model=List[dict]
-)
-async def get_a_list_of_registered_users(
-    only_active: bool = False,
-    db: Session = Depends(get_db)
-):  
-    try:
-        response = uu.get_all_usernames(db)
-        return response
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    
-
-@admin_router.put(
-    '/disable_account',
-    summary="Disable an active account",
-    response_model=schemas.User
-)
-async def disable_account(
-    username_or_email: Annotated[str, "Exact username"]
-):  
-    updated_user = await set_disabled(
-        username_or_email, disable=True
-    )
-    return updated_user
-
-
-@admin_router.put(
-    '/enable_account',
-    summary="Enable a disactive account",
-    response_model=schemas.User
-)
-async def disable_account(
-    username_or_email: Annotated[str, "Exact username"]
-):  
-    updated_user = await set_disabled(
-        username_or_email, disable=False
-    )
-    return updated_user
-
-
-@admin_router.get(
-    '/db_sql_file',
+    '/get_from_downloadables',
     response_class=fastapi.responses.FileResponse
 )
-async def download_sql_file_of_app_db(
-    path_rel_to_session_folder="teledash.db"
+async def get_file_from_downloadable_folder(
+    path_rel_to_downloadable_folder="teledash.db"
 ):
     
-    file_path = os.path.join(config.SESSIONS_FOLDER, path_rel_to_session_folder)
-    if not os.path.isfile(file_path):
-        raise HTTPException(status_code=400, detail=f"File {file_path} does not exist")
+    file_path = Path(config.DATA_FOLDER).joinpath('downloadables', path_rel_to_downloadable_folder)
+    if not file_path.is_file():
+        raise HTTPException(
+            status_code=400, detail=f"File {file_path} does not exist or is not a file")
     return fastapi.responses.FileResponse(file_path)
     
