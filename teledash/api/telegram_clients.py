2dca0de1dd05cde5ae5b365d9283221fd51b9fc3
Giosuè Ruscica <dellarovere.lante@gmail.com>
2024-10-30 11:06:38 +0100

    Decrypt data and add loggings

--- teledash/api/telegram_clients.py
+++ teledash/api/telegram_clients.py
@@ -4,11 +4,16 @@ from teledash.db.db_setup import get_async_session
 from teledash.db import models
 from teledash.utils.users import active_user
 from teledash.utils.db import user as uu
-from teledash.utils.db import channel as uc
 from teledash.utils.db import tg_client as ut
 from teledash.utils import telegram
-from teledash import schemas
 from typing import Annotated
+import jwt
+from teledash.config import settings
+from teledash.utils.admin import encrypt_data, decrypt_data
+import logging
+
+
+logger = logging.getLogger('uvicorn.error')
 
 
 clients_router = fastapi.APIRouter()
@@ -25,6 +30,15 @@ async def add_phone_to_user(
     db: Session = fastapi.Depends(get_async_session),
     phone: bool = True
 ):
+    
+    key_cookie = jwt.decode(
+        request.cookies.get("key_hash"),
+        settings.JWT_SECRET_KEY, 
+        algorithms=[settings.ALGORITHM],
+    ).get("sub")
+    
+    enc_key_encrypted = bytes.fromhex(key_cookie)
+    enc_key = bytes.fromhex(decrypt_data(settings.DATA_SECRET_KEY.encode(), enc_key_encrypted))
     if phone:
         client_creation_func = telegram.create_client
         phone_or_bot_token = telegram.parse_phone(phone_or_bot_token)
@@ -43,51 +57,56 @@ async def add_phone_to_user(
         authenticated = client_in_db.get("authenticated", False)
         
         # authenticated = await client_is_logged_and_usable(client_id, api_id, api_hash)
-        print("authenticated: ", authenticated)
+        logger.info(f"Client of phone {phone} authentication: {str(authenticated)}")
         try:
             client_used_by_app = request.app.state.clients.get(client_id)
         except Exception:
             client_used_by_app = None
         if client_used_by_app:
-            print("[add_tg_phone]: client in app")
+            logger.info(f"Client of phone {phone} is being used by app")
             await ut.upsert_user_client_relation(
                 db=db, user_id=user.id, client_id=client_id
             )
             is_usable = await telegram.client_is_logged_and_usable(client_used_by_app)
-            print("[add_tg_phone] is usable: ", is_usable)
+            logger.info(f"Client of phone {phone} is usable: {str(is_usable)}")
             if is_usable:
                 return client_in_db
             else:
                 authenticated = False
         else:
             authenticated = False
-        print("[register]: creating client")   
         client_dict = await client_creation_func(
             phone_or_bot_token, api_id, api_hash, code, authenticated,
         )
-        print("[register]: created")
+        logger.info(f"Created Client for phone {phone}") 
+        
+        enc_api_id = encrypt_data(enc_key, str(api_id))
+        enc_api_hash = encrypt_data(enc_key, api_hash)
+        enc_phone = encrypt_data(enc_key, phone_or_bot_token)
         tg_client = {
             "id": client_dict["session_file"],
-            "phone": phone_or_bot_token,
-            "api_id": api_id,
-            "api_hash": api_hash
+            "phone": enc_phone.hex(),
+            "api_id": enc_api_id.hex(),
+            "api_hash": enc_api_hash.hex()
         }
         tg_client["authenticated"] = True if (client_dict["status"] == "ok") else False    
         await ut.upsert_tg_client(db=db, row_dict=tg_client)
         await ut.upsert_user_client_relation(
             db=db, user_id=user.id, client_id=client_id
         )
-        print("inserted")
+        logger.info(f"Inserted Client in database for phone {phone}") 
+
         if tg_client["authenticated"]:
             request.app.state.clients[client_id] = await telegram.get_authenticated_client(
-                db, client_id
+                db, client_id, enc_key
             )
-            print("set")
+            logger.info(f"Added client to App clients for phone {phone}") 
+
     except Exception as e:
         raise fastapi.HTTPException(
             status_code=400, detail=[{"msg": str(e)}]
         )
-    print("outing")
+    tg_client["phone"] = phone_or_bot_token  # return decrypted phone 
     return tg_client
 
 
